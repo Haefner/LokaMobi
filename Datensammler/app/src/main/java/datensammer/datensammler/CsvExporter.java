9cac877d0ba6b43ca57bcacd234d4b27a66aeaf1
56485 <oliver.bleck@hs-bochum.de>
2018-11-19 12:16:57 +0100

    Refactor durch lambda.

--- Datensammler/app/src/main/java/datensammer/datensammler/CsvExporter.java
+++ Datensammler/app/src/main/java/datensammer/datensammler/CsvExporter.java
@@ -80,11 +80,9 @@ public class CsvExporter {
 
             MediaScannerConnection.scanFile(context,
                     new String[] { file.toString() }, null,
-                    new MediaScannerConnection.OnScanCompletedListener() {
-                        public void onScanCompleted(String path, Uri uri) {
-                            Log.i("ExternalStorage", "Scanned " + path + ":");
-                            Log.i("ExternalStorage", "-> uri=" + uri);
-                        }
+                    (path1, uri) -> {
+                        Log.i("ExternalStorage", "Scanned " + path1 + ":");
+                        Log.i("ExternalStorage", "-> uri=" + uri);
                     });
 
         } catch (IOException e) {
